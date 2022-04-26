f0b3cd44c3aae5a753fbc8031c232f4c01ef9bfa
AlexandrLavrou <alex_lavrov@inbox.ru>
2022-04-27 00:45:11 +0300

    add methods function

--- candidat.py
+++ candidat.py
@@ -1,3 +1,4 @@
+
 class Candidates:
 
     def __init__(self, ident, name, picture, position, skills):
@@ -10,17 +11,21 @@ class Candidates:
     def __repr__(self):
         return f"{self.name}\n{self.position}\n{self.skills}\n\n"
 
-    
+    def get_candy_name(self):
+        """
+        метод возвращает имя кандидата в запланированном виде
+        """
+        return f" {self.name}"
 
     def get_candy_info(self):
         """
         метод возвращает данные кандидата в запланированном виде
-        :return:
         """
         return f"<pre>\n" \
-               f"<h3>Имя кандидата - {self.name}\n" \
-               f"Позиция {self.position}\n" \
-               f"Навыки: {self.skills}\n</h3>" \
+               f"<h1>Имя кандидата - {self.name}</h1>\n" \
+               f"<p>Позиция {self.position}</p>\n" \
+               f"<p><img src= {self.pictures} width=200/></p>\n" \
+               f"<p>Навыки: {self.skills}</p>\n" \
                f"</pre>\n\n"
 
     def get_foto_candy(self):
@@ -37,7 +42,7 @@ class Candidates:
         :return: возвращает фото и данные в нужном формате
         """
         if int(input_id) == int(self.ident):
-            return self.get_foto_candy() + self.get_candy_info()
+            return self.get_candy_info()
 
     def chek_candy_skill(self, input_skill):
         """
@@ -48,6 +53,34 @@ class Candidates:
         temp_skills = self.skills.split(", ")
         for skill in temp_skills:
             if input_skill.lower() == skill.lower():
+                get_candy_with_skill = self.get_candy_info()
+            else:
+                continue
+            return get_candy_with_skill
+
+    def check_candy_name(self, candidate_name):
+        """
+        Метод проверяет навык в списке и при совпадении возвращает данные кандидата
+        :param candidate_name: проверяемое имя или его часть
+        :return: данные кандидата с запрашиваемым именем
+        """
+        temp_names = self.name.split(" ")
+        for name in temp_names:
+            if candidate_name.lower() in name.lower():
+                get_candy_with_name = self.get_candy_info()
+            else:
+                continue
+            return get_candy_with_name
+
+    def check_candy_skill(self, skill_name):
+        """
+        Метод проверяет навык в списке и при совпадении возвращает данные кандидата
+        :param skill_name: входящий проверяемый навык
+        :return: данные кандидата с запрашиваемым навыком
+        """
+        temp_skills = self.skills.split(", ")
+        for skill in temp_skills:
+            if skill_name.lower() == skill.lower():
                 get_candy_with_skill = self.get_candy_info()
             else:
                 continue
--- utils.py
+++ utils.py
@@ -1,14 +1,82 @@
 
-# - `load_candidates_from_json(path)` – возвращает список всех кандидатов
-# - `get_candidate(candidate_id)` – возвращает одного кандидата по его id
-# - `get_candidates_by_name(candidate_name)` – возвращает кандидатов по имени
-# - `get_candidates_by_skill(skill_name)` – возвращает кандидатов по навыку
 import requests
+
+import candidat
 
 
 def load_candidates_from_json(path):
+    """
+    Функция загружает из файла JSON список из
+    экземпляров класса Candidates
+    :return:
+    """
     _candidates = requests.get(path).json()
-    candydates_inclass = []
+    candidates_in_class = []
     for _ in _candidates:
+        ident = _.get("id")
+        name = _.get("name")
+        pictures = _.get("picture")
+        position = _.get("position")
+        skills = _.get("skills")
+
+        candidate = candidat.Candidates(ident, name, pictures, position, skills)
+        candidates_in_class.append(candidate)
+
+    return candidates_in_class
 
 
+def load_candidates_names(path):
+    """
+    Функция загружает из файла JSON список имена кандидатов
+    """
+    _candidates = load_candidates_from_json(path)
+    name_list = []
+    for _c in _candidates:
+        name_list.append(_c.get_candy_name())
+    return name_list
+
+
+def get_candidate(path, candidate_id):
+    """
+    Функция загружает из файла список сущностей класса Candidates
+     и возвращает данные кандидата по ID
+    :param candidate_id: Входящий ID
+    :param path: путь к файлу Json
+    :return: возвращает данные кандидата с запрашиваемым ID в рекомендуемой форме
+    """
+    _candidates = load_candidates_from_json(path)
+    for _c in _candidates:
+        if _c.chek_candy_from_id(candidate_id):
+            return _c.chek_candy_from_id(candidate_id)
+
+
+def get_candidates_by_name(path, candidate_name):
+    """
+    Функция загружает из файла список сущностей класса Candidates
+     и возвращает данные кандидатов по имени или его части
+    :param path: путь к файлу Json
+    :param candidate_name: имя кандидата введенного пользователем
+    :return: возвращает список кандидатов по имени или его части
+    """
+    _candidates = load_candidates_from_json(path)
+    candy_with_name = ""
+    for _c in _candidates:
+        if _c.check_candy_name(candidate_name):
+            candy_with_name += _c.check_candy_name(candidate_name)
+    return candy_with_name
+
+
+def get_candidates_by_skill(path, skill_name):
+    """
+    Функция загружает из файла список сущностей класса Candidates
+    и формирует список кандидатов с запрашиваемыми навыками в заданном формате
+    :param path: путь к файлу Json
+    :param skill_name: входящий навык
+    :return: возвращает список кандидатов с запрашиваемым навыком
+    """
+    _candidates = load_candidates_from_json(path)
+    candy_with_skill = ""
+    for _c in _candidates:
+        if _c.check_candy_skill(skill_name):
+            candy_with_skill += _c.check_candy_skill(skill_name)
+    return candy_with_skill
